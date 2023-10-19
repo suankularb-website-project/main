99e27ea6257a94e729e5adc6d9e4f24e9be2b31e
AungS8430 <aungshome@outlook.com>
2023-10-19 20:11:37 +0700

    Complete about contact page

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Sidebar from "../../components/about";
 
 export default function AboutLayout({ children }: { children: React.ReactNode }) {
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { Menu } from "antd";
+import { Card } from "antd";
 
 export default function Contact() {
 
@@ -8,6 +8,13 @@ export default function Contact() {
         <main className="flex flex-col items-center justify-between top-0 pt-0">
             <div className="container max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg flex flex-col items-center justify-center py-10 px-10 gap-2" id="content">
                 <iframe width={"100%"} height={300} id="gmap_canvas" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3875.5856038339907!2d100.49621977423354!3d13.74352058664747!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e2991b9e08e2dd%3A0x5158736cefddaf8f!2sSuankularb%20Wittayalai%20School!5e0!3m2!1sen!2sth!4v1697638403379!5m2!1sen!2sth" />
+                <div className="flex flex-col items-start justify-start gap-2 pt-3">
+                    <h1 className="text-2xl font-bold">ติดต่อ</h1>
+                    <p className="text-lg font-normal text-start">โรงเรียนสวนกุหลาบวิทยาลัย 88 ถนนตรีเพชร แขวงวังบูรพาภิรมย์ เขตพระนคร กรุงเทพมหานคร 10200</p>
+                    <p className="text-lg font-normal text-start">โทรศัพท์ 02-2216701, 02-2255605-8</p>
+                    <p className="text-lg font-normal text-start">โทรสาร 02-2248554</p>
+                    <p className="text-lg font-normal text-start">รหัสสถานศึกษา : 00100101</p>
+                </div>
             </div>
         </main>
     )
