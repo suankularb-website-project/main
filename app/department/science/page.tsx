c58b5a714f171b84a1e8fc12715d7c5b358c8606
AungS8430 <aungshome@outlook.com>
2023-10-23 18:03:22 +0700

    Remove image optimiztion on problematic pages

--- app/department/science/page.tsx
+++ app/department/science/page.tsx
@@ -13,7 +13,7 @@ export default function Department() {
                                 key={index}
                                 hoverable
                                 className="w-full bg-transparent h-[15rem] lg:h-80"
-                                cover={<div className="h-[10.75rem] lg:h-60 justify-center"><Image alt={item.name} src={item.cover} width={900} height={200} className="w-auto h-full mx-auto" /></div>}
+                                cover={<div className="h-[10.75rem] lg:h-60 justify-center"><Image unoptimized alt={item.name} src={item.cover} width={900} height={200} className="w-auto h-full mx-auto" /></div>}
                                 title={<div><h3>{item.name}</h3><p>{item.type}</p></div>}  
                             >
                             </Card>
--- next.config.js
+++ next.config.js
@@ -16,18 +16,6 @@ const nextConfig = {
 			},
 			{
 				protocol: 'https',
-				hostname: 'lh4.googleusercontent.com'
-			},
-			{
-				protocol: 'https',
-				hostname: 'lh5.googleusercontent.com'
-			},
-			{
-				protocol: 'https',
-				hostname: 'lh6.googleusercontent.com'
-			},
-			{
-				protocol: 'https',
 				hostname: 'drive.google.com'
 			}
         ],
