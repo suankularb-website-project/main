aca9334b59375f95128c2ae01c3c9e36cab95be1
AungS8430 <aungshome@outlook.com>
2023-10-23 18:31:16 +0700

    Opted static storage

--- next.config.js
+++ next.config.js
@@ -10,24 +10,10 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'i.ibb.co'
             },
-			{
-				protocol: 'https',
-				hostname: 'lh3.googleusercontent.com'
-			},
-			{
-				protocol: 'https',
-				hostname: 'drive.google.com'
-			}
         ],
 		domains: [
 			'96.30.126.41',
 			'i.ibb.co',
-			'lh3.googleusercontent.com',
-			'lh4.googleusercontent.com',
-			'lh5.googleusercontent.com',
-			'lh6.googleusercontent.com',
-			'drive.google.com',
-			'googleusercontent.com'
 		]
     }
 }
