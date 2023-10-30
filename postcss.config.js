46f3c3ccc27ff7600d781ee44e77ce597fb97e85
AungS8430 <aungshome@outlook.com>
2023-10-30 19:57:34 +0700

    Fix CSS conflict, updated to Next.js 14

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,6 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    'postcss-antd-fixes': { prefixes: ['vp-antd', 'ant'] },
   },
 }
---
+++ theme/themeConfig.ts
@@ -0,0 +1,48 @@
+import type { ThemeConfig } from 'antd';
+import { theme } from 'antd';
+
+const themed: ThemeConfig = {
+    algorithm: theme.darkAlgorithm,
+    components: {
+        Card: {
+            headerBg: "#101010",
+            colorBgContainer: "#101010",
+            colorTextHeading: "#ffffff",
+            algorithm: true
+        },
+        Pagination: {
+            colorText: "#ffffff",
+            colorTextDisabled: "#ffffff",
+            algorithm: true
+        },
+        Input: {
+            colorBgContainer: "#000000",
+            colorBorder: "#ffffff",
+            colorText: "#ffffff",
+            colorTextPlaceholder: "#909090",
+            algorithm: true
+        },
+        Menu: {
+            itemBg: "#000000",
+            itemActiveBg: "#101010",
+            itemColor: "#ffffff",
+            itemHoverColor: "#f5f5f5",
+            subMenuItemBg: "#101010",
+            popupBg: "#000000",
+            darkSubMenuItemBg: "#101010",
+            darkItemBg: "#000000",
+            darkItemColor: "#ffffff",
+            algorithm: true
+        },
+        Table: {
+            headerBg: "#101010",
+            headerColor: "#ffffff",
+            borderColor: "#303030",
+            colorBgContainer: "#050505",
+            colorText: "#f9f9f9",
+            algorithm: true
+        },
+    },
+};
+
+export default themed;
