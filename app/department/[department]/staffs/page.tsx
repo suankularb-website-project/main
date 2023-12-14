36b4408fe060530c203f5406a0895870b1a8857f
AungS8430 <aungshome@outlook.com>
2023-12-14 19:44:38 +0700

    Image optimization.

--- app/department/[department]/staffs/page.tsx
+++ app/department/[department]/staffs/page.tsx
@@ -31,19 +31,19 @@ export default function Department( { params }: { params: { department: string }
                             key={0}
                             hoverable
                             className="bg-transparent h-[15rem] lg:h-80"
-                            cover={<div className="h-[10.75rem] lg:h-60 justify-center"><Image alt={head.name} src={head.cover} width={900} height={200} className="w-auto h-full mx-auto" /></div>}
+                            cover={<div className="h-[10.75rem] lg:h-60 justify-center"><Image alt={head.name} src={head.cover} width={900} height={200} priority className="w-auto h-full mx-auto" /></div>}
                             title={<div><h3>{head.name} หัวหน้ากลุ่มสาระการเรียนรู้ฯ</h3><p>{head.type}</p></div>}  
                         />
                     </center>
                 </div>
-                <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+                <div className="w-full grid grid-cols-1 xxs:grid-cols-2 lg:grid-cols-3 gap-2">
                     {department.staffs.slice(1).map((item: any, index: any) => (
                         <center key={index}>
                             <Card
                                 key={index}
                                 hoverable
                                 className="w-full bg-transparent h-[15rem] lg:h-80"
-                                cover={<div className="h-[10.75rem] lg:h-60 justify-center"><Image alt={item.name} src={item.cover} width={900} height={200} className="w-auto h-full mx-auto" /></div>}
+                                cover={<div className="h-[10.75rem] lg:h-60 justify-center"><Image alt={item.name} src={item.cover} width={340} height={250} className="w-auto h-full mx-auto" /></div>}
                                 title={<div><h3>{item.name}</h3><p>{item.type}</p></div>}  
                             />
                         </center>
